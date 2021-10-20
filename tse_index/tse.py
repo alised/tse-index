f26db099bcb6253c5d494ff86806da51be83ccd1
alised <seddighi.ali@gmail.com>
2021-10-20 23:17:40 +0330

    test

--- tse_index/tse.py
+++ tse_index/tse.py
@@ -205,6 +205,8 @@ class reader:
             resp = self.client.DecompressAndGetInsturmentClosingPrice(
                 ";".join(insCodesList[chunk : chunk + self.chunksize])
             )
+            print(insCodesList)
+            print(resp)
             historyStr = resp.split("@")
             for i, v in enumerate(historyStr):
                 data = StringIO(v)
